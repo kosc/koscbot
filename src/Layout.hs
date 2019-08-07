b2d9e32749979adb4cdc03355b7ab2aea5a81f2b
Artem Kosenko <artem.kosenko@lgepartner.com>
2019-08-07 14:06:34 +0300

    Layout hotfix

--- src/Layout.hs
+++ src/Layout.hs
@@ -3,7 +3,7 @@ module Layout (changeLayout) where
 import qualified Data.Map as Map
 import Data.Maybe (fromMaybe)
 
-mapping = Map.fromList $ zip "qwertyuiop[]asdfghjkl;'\\zxcvbnm,./QWERTYUIOP[]ASDFGHJKL:\"|ZXCVBNM<>?" "йцукенгшщзхъфывапролджэ\\ячсмитьбю.ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭ/ЯЧСМИТЬБЮ,"
+mapping = Map.fromList $ zip "qwertyuiop[]asdfghjkl;'\\zxcvbnm,./QWERTYUIOP{}ASDFGHJKL:\"|ZXCVBNM<>?" "йцукенгшщзхъфывапролджэ\\ячсмитьбю.ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭ/ЯЧСМИТЬБЮ,"
 
 findChar c = let l = Map.lookup c mapping
              in case l of
